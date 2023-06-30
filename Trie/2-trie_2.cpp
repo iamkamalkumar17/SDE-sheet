7d6b04947eb80d46be4814a511d842e63a5a486f
Kamal Kumar <88319916+iamkamalkumar17@users.noreply.github.com>
2023-06-30 17:04:18 +0530

    Create 2-trie_2.cpp

---
+++ Trie/2-trie_2.cpp
@@ -0,0 +1,149 @@
+/*
+Insert
+Here we will maintain ew -> end with  and  cp -> count prefix,
+We will insert letter by letter by creating a new track every time. Starting from the root, 
+fill letters of the word one by one and subsequently increase cp. At the end of every word, 
+increase ew which denotes that word finished here. At the time a different prefix is found, 
+we will create a new track by increasing cp.
+
+2. countwordsEqualTo() – This function will count complete words which are present in the Trie. 
+Starting from the root, we will check for the given letter of the word to be found, if we find 
+that cp >0 every time and at the end if (ew>0) this means word ends here means the complete word is present here.
+
+3. countWordsStartWith() – This function will count words starting with given string. Similarly, 
+here also we begin from the root and check for the given letter one by one and check for cp>0. If it is, 
+this means the given words are present here.
+
+4. Erase() – It will delete the string given from the Trie.
+
+Note- In Erase() function, it is assumed that the given letter to be erased is present in the trie.
+
+Starting from the root, cp for the given prefix element is greater than 0, reduce cp by 1 and move 
+on to a new track, and at last, reduce ew by 1  to mark that. word is fully deleted.
+
+
+*/
+  '
+  #include <bits/stdc++.h>
+
+using namespace std;
+struct Node {
+  Node * links[26];
+  int cntEndWith = 0;
+  int cntPrefix = 0;
+
+  bool containsKey(char ch) {
+    return (links[ch - 'a'] != NULL);
+  }
+  Node * get(char ch) {
+    return links[ch - 'a'];
+  }
+  void put(char ch, Node * node) {
+    links[ch - 'a'] = node;
+  }
+  void increaseEnd() {
+    cntEndWith++;
+  }
+  void increasePrefix() {
+    cntPrefix++;
+  }
+  void deleteEnd() {
+    cntEndWith--;
+  }
+  void reducePrefix() {
+    cntPrefix--;
+  }
+  int getEnd() {
+    return cntEndWith;
+  }
+  int getPrefix() {
+    return cntPrefix;
+  }
+};
+class Trie {
+  private:
+    Node * root;
+
+  public:
+    /** Initialize your data structure here. */
+    Trie() {
+      root = new Node();
+    }
+
+  /** Inserts a word into the trie. */
+  void insert(string word) {
+    Node * node = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (!node -> containsKey(word[i])) {
+        node -> put(word[i], new Node());
+      }
+      node = node -> get(word[i]);
+      node -> increasePrefix();
+    }
+    node -> increaseEnd();
+  }
+
+ int countWordsEqualTo(string &word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return node->getEnd();
+    }
+
+
+  int countWordsStartingWith(string & word) {
+    Node * node = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (node -> containsKey(word[i])) {
+        node = node -> get(word[i]);
+      } else {
+        return 0;
+      }
+    }
+    return node -> getPrefix();
+  }
+
+  void erase(string & word) {
+    Node * node = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (node -> containsKey(word[i])) {
+        node = node -> get(word[i]);
+        node -> reducePrefix();
+      } else {
+        return;
+      }
+    }
+    node -> deleteEnd();
+  }
+};
+
+int main() {
+  Trie T;
+  T.insert("apple");
+  T.insert("apple");
+  T.insert("apps");
+  T.insert("apps");
+  string word1 = "apps";
+  cout<<"Count Words Equal to "<< word1<<" "<<T.countWordsEqualTo(word1)<<endl;
+  string word2 = "abc";
+  cout<<"Count Words Equal to "<< word2<<" "<<T.countWordsEqualTo(word2)<<endl;
+  string word3 = "ap";
+  cout<<"Count Words Starting With "<<word3<<" "<<T.countWordsStartingWith(word3)
+  <<endl;
+  string word4 = "appl";
+  cout<<"Count Words Starting With "<< word4<<" "<<T.countWordsStartingWith(word4)
+  <<endl;
+  T.erase(word1);
+  cout<<"Count Words equal to "<< word1<<" "<<T.countWordsEqualTo(word1)<<endl;
+  return 0;
+}
